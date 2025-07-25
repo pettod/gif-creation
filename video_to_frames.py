e2dd47eff4c712549c5c65d46d9f044e41e7b47a
Peter Todorov <peter.todorov@live.com>
2025-07-24 17:53:29 -0700

    Fix the ffmpeg running error

--- video_to_frames.py
+++ video_to_frames.py
@@ -10,5 +10,5 @@ args = parser.parse_args()
 # Create folder and extract frames
 video_path = args.video_path
 video_name = video_path.split("/")[-1].split(".")[0] + "_all-frames"
-os.mkdir(video_name)
-os.system("ffmpeg -i vid.webm {}/%05d.png".format(video_name))
+os.makedirs(video_name, exist_ok=True)
+os.system("ffmpeg -i {} {}/%05d.png".format(video_path, video_name))
